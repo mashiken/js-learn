7f98bd35bc5f328aed2d0661d371c5ae5562f501
mashiken <tatsumikogorou@gmail.com>
2020-10-04 23:33:12 +0900

    Learn array

---
+++ Lesson8.js
@@ -0,0 +1,34 @@
+//  配列 1
+var a = new Array(3);
+
+a[0] = "Ken Nakanowatari";
+a[1] = "Akira Oowada";
+a[2] = "Naoki Hanzawa";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+
+var b = new Array("Taro","Jiro","Saburo");
+
+console.log(b[0]);
+console.log(b[1]);
+console.log(b[2]);
+
+// 配列2
+var c = ["hanako","tomiko","masako"];
+
+c[1] = "sizuko";
+
+console.log(c[0]);
+console.log(c[1]);
+console.log(c[2]);
+
+// 二次元配列
+var arr;
+arr = [["Ken nakanowatari","todori"],["Youitirou Mikasa","hukutoudori"]];
+
+console.log(arr[0][0]);
+console.log(arr[0][1]);
+console.log(arr[1][0]);
+console.log(arr[1][1]);
