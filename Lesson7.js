c5ad399abf0106ff3bf645c06ea9bcdd7bcaf60d
mashiken <tatsumikogorou@gmail.com>
2020-10-04 23:19:29 +0900

    Learn data types

---
+++ Lesson7.js
@@ -0,0 +1,21 @@
+//  数値型
+var num01 = 123;
+var num02 = 1.23;
+console.log(typeof(num01));
+console.log(typeof(num02));
+
+//  文字列型
+var string_a = "Hello,World!";
+
+console.log(string_a);
+console.log(typeof(string_a));
+
+//   ブール型
+var a = 10;
+var b = 1;
+var bool01;
+
+bool01 = (a > b);
+console.log(bool01);
+console.log(typeof(bool01));
+
