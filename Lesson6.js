db8807cf4415d2d70910e04622ad2421d6aa33e0
mashiken <tatsumikogorou@gmail.com>
2020-10-04 23:07:52 +0900

    Learn variable

---
+++ Lesson6.js
@@ -0,0 +1,20 @@
+//  変数
+var num = 1;
+console.log(num);
+
+// 変数2
+
+var num = 1;
+var num01 = 2;
+var num_01 = 3;
+var num$01 = 4;
+
+console.log(num);
+console.log(num01);
+console.log(num_01);
+console.log(num$01);
+// 変数 大文字・小文字は識別する
+var NUM = 1;
+var Num = 2;
+console.log(num);
+console.log(Num);
